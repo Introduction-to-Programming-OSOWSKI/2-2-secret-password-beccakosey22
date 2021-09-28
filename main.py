5a9a31f13fde9f827081d453a00e1e4590283dec
beccakosey22 <820512@mystma.org>
2021-09-28 09:03:41 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-#WRITE YOUR CODE IN THIS FILE
+#Create a function password() that returns access granted/denied if a given variable p equals the secret password "Knights19"
+def password(p):
+    if p == "Knights19":
+        return "ACCESS GRANTED"
+    else:
+        return "ACCESS DENIED"
